b354d25616ea7087e69cea7fe5e00d6a1dc6252d
Tom Zhao <zhaoxuan0914@hotmail.com>
2020-12-15 11:43:36 +0800

    added benchmarking script: stats.sh

--- stats.sh
+++ stats.sh
@@ -3,5 +3,6 @@
 for i in {1..1000}
 do
     echo "The $i round starts to run!"
-    ./Main clever-ai clever-ai --strategy1 PlanetRankRush --strategy2 ZergRush --headless >> stats.output
+    ./Main clever-ai clever-ai --strategy1 Skynet --strategy2 ZergRush --headless --no-recomp >> against_zergrush.output
+    ./Main clever-ai clever-ai --strategy1 Skynet --strategy2 PlanetRankRush --headless --no-recomp >> against_planetrank.output
 done
