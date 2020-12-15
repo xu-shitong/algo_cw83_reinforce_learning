1bd8c02a9b3a56d7cb0ac1296d2b061c1b859b33
Tom Zhao <zhaoxuan0914@hotmail.com>
2020-12-15 13:02:21 +0800

    added benchmarking script: stats.sh

--- stats.sh
+++ stats.sh
@@ -6,3 +6,19 @@ do
     ./Main clever-ai clever-ai --strategy1 Skynet --strategy2 ZergRush --headless --no-recomp >> against_zergrush.output
     ./Main clever-ai clever-ai --strategy1 Skynet --strategy2 PlanetRankRush --headless --no-recomp >> against_planetrank.output
 done
+
+echo "The stats for Skynet playing against ZergRush"
+echo "Number of time we won: "
+grep "Player 1" against_zergrush.output | wc -l
+echo "Number of time they won: "
+grep "Player 2" against_zergrush.output | wc -l
+echo "Number of draws: "
+grep "Draw" against_zergrush.output | wc -l
+
+echo "The stats for Skynet playing against PlanetRankRush"
+echo "Number of time we won: "
+grep "Player 1" against_planetrank.output | wc -l
+echo "Number of time they won: "
+grep "Player 2" against_planetrank.output | wc -l
+echo "Number of draws: "
+grep "Draw" against_planetrank.output | wc -l
