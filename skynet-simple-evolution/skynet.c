3e0cdd9fb9cfc72fe2a066d9bb78b5d17b2b7d24
shitong xu <tonyshengxue@outlook.com>
2021-08-30 16:57:43 +0100

    xst: 16:57 30/08: retry remove unused files

--- skynet-simple-evolution/skynet.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "train.h"
-#include <stdlib.h>
-
-int main(void) {
-
-  train();
-  return EXIT_SUCCESS;
-}
--- skynet/skynet.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-#include "train.h"
-
-int main(void) {
-
-}
